fbe31960bdf8ea5f20d4e623f556642aa55528d4
winterwoods17 <akuma.shirin@gmail.com>
2025-05-29 01:02:13 +0530

    forst comment

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals("Hello", "Hello");
+    }
+}
